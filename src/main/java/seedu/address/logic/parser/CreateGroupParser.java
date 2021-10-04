8a504ad6be31e060d4bbbcc3fc57f4b1db05792d
Ramapriyan Srivatsan P.D <ramapriyan912001@gmail.com>
2021-10-04 09:43:38 +0800

    Update CreateGroupParser.java
    Move GroupName import above Name to maintain lexicographical order.

--- src/main/java/seedu/address/logic/parser/CreateGroupParser.java
+++ src/main/java/seedu/address/logic/parser/CreateGroupParser.java
@@ -2,8 +2,8 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CreateGroupCommand.MESSAGE_EMPTY_GROUP;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
 import java.util.ArrayList;
 import java.util.Objects;
