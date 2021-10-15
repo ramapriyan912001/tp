40a1a509d415d26db2c49639f3911b9b3b74dad8
marcuspeh <hkmpeh@gmail.com>
2021-10-15 09:44:42 +0800

    Fix checkstyle error

--- src/main/java/seedu/awe/model/util/SampleDataUtil.java
+++ src/main/java/seedu/awe/model/util/SampleDataUtil.java
@@ -25,36 +25,36 @@ import seedu.awe.model.tag.Tag;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
-                new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                        new Address("Blk 30 Geylang Street 29, #06-40"),
-                        getTagSet("friends")),
-                new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                        new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                        getTagSet("colleagues", "friends")),
-                new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                        new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                        getTagSet("neighbours")),
-                new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                        new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                        getTagSet("family")),
-                new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                        new Address("Blk 47 Tampines Street 20, #17-35"),
-                        getTagSet("classmates")),
-                new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                        new Address("Blk 45 Aljunied Street 85, #11-31"),
-                        getTagSet("colleagues"))
+            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
+                    new Address("Blk 30 Geylang Street 29, #06-40"),
+                    getTagSet("friends")),
+            new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
+                    new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
+                    getTagSet("colleagues", "friends")),
+            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
+                    new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+                    getTagSet("neighbours")),
+            new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
+                    new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
+                    getTagSet("family")),
+            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
+                    new Address("Blk 47 Tampines Street 20, #17-35"),
+                    getTagSet("classmates")),
+            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
+                    new Address("Blk 45 Aljunied Street 85, #11-31"),
+                    getTagSet("colleagues"))
         };
     }
 
     public static Expense[] getSampleExpenses() {
         Person[] members = getSamplePersons();
         return new Expense[] {
-                new Expense(members[0], new Cost("10"), new Description("Transportation")),
-                new Expense(members[1], new Cost("300"), new Description("Buffet")),
-                new Expense(members[2], new Cost("60"), new Description("Souvenirs")),
-                new Expense(members[3], new Cost("200"), new Description("Entry Tickets")),
-                new Expense(members[3], new Cost("150"), new Description("Lunch")),
-                new Expense(members[4], new Cost("180"), new Description("Supper"))
+            new Expense(members[0], new Cost("10"), new Description("Transportation")),
+            new Expense(members[1], new Cost("300"), new Description("Buffet")),
+            new Expense(members[2], new Cost("60"), new Description("Souvenirs")),
+            new Expense(members[3], new Cost("200"), new Description("Entry Tickets")),
+            new Expense(members[3], new Cost("150"), new Description("Lunch")),
+            new Expense(members[4], new Cost("180"), new Description("Supper"))
         };
     }
 
@@ -65,10 +65,10 @@ public class SampleDataUtil {
         ArrayList<Expense> baliExpenses = addSampleExpenses(3, 6);
 
         return new Group[] {
-                new Group(new GroupName("London"), londonMembers, getTagSet("SchoolTrip"),
-                        londonExpenses),
-                new Group(new GroupName("Bali"), baliMembers, getTagSet("3DayTrip"),
-                        baliExpenses)
+            new Group(new GroupName("London"), londonMembers, getTagSet("SchoolTrip"),
+                    londonExpenses),
+            new Group(new GroupName("Bali"), baliMembers, getTagSet("3DayTrip"),
+                    baliExpenses)
         };
     }
 
