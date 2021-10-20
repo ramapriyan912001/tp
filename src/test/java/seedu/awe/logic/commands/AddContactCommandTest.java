ea0c0130542255ad08327e0f30b435b8a69e7fa3
marcuspeh <hkmpeh@gmail.com>
2021-10-20 20:25:06 +0800

    Fix checkstyle

--- src/test/java/seedu/awe/logic/commands/AddContactCommandTest.java
+++ src/test/java/seedu/awe/logic/commands/AddContactCommandTest.java
@@ -149,7 +149,7 @@ public class AddContactCommandTest {
         public void setTransactionSummary(HashMap<Person, Cost> summary) {
             throw new AssertionError("This method should not be called.");
         }
-      
+
         @Override
         public void setAllMembersOfGroup(Group group) {
             throw new AssertionError("This method should not be called.");
