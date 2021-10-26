caaab0f520d0e6899689890fe93940d7bc167899
Jingwencloud <jingwen098765432@gmail.com>
2021-10-26 18:49:31 +0800

    Add help message pop-up

--- src/main/java/seedu/awe/ui/Ui.java
+++ src/main/java/seedu/awe/ui/Ui.java
@@ -10,4 +10,7 @@ public interface Ui {
     /** Starts the UI (and the App).  */
     void start(Stage primaryStage);
 
+    /** Sets the boolean indicating if there was an error reading from storage. */
+    void setIsDataError(boolean isDataError);
+
 }
