5c84f1cde003e031f0637530678e141f19581c5e
ramapriyan912001 <ramapriyan912001@gmail.com>
2021-10-27 22:18:09 +0800

    Edit calculatepaymentscommand test

--- src/test/java/seedu/awe/logic/commands/CalculatePaymentsCommandTest.java
+++ src/test/java/seedu/awe/logic/commands/CalculatePaymentsCommandTest.java
@@ -1,5 +1,17 @@
 package seedu.awe.logic.commands;
 
+import seedu.awe.model.Model;
+import seedu.awe.testutil.ModelBuilder;
+import seedu.awe.testutil.TypicalGroups;
+
 public class CalculatePaymentsCommandTest {
+
+    private Model model = new ModelBuilder().build();
+
+    public void resetModel() {
+        model = new ModelBuilder().build();
+    }
+
+    CalculatePaymentsCommand calculatePaymentsCommand = new CalculatePaymentsCommand(TypicalGroups.BALI);
 
 }
