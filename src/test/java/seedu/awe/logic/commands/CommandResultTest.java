1d2123ee570ba8cf47a0f58915f3dea476ee07c5
marcuspeh <hkmpeh@gmail.com>
2021-11-01 10:49:21 +0800

    Update command result test

--- src/test/java/seedu/awe/logic/commands/CommandResultTest.java
+++ src/test/java/seedu/awe/logic/commands/CommandResultTest.java
@@ -28,6 +28,11 @@ public class CommandResultTest {
 
         // different feedbackToUser value -> returns false
         assertFalse(commandResult.equals(new CommandResult("different")));
+    }
+
+    @Test
+    public void equals_withUiTest_failure() {
+        CommandResult commandResult = new CommandResult("feedback");
 
         // different showHelp value -> returns false
         assertFalse(commandResult.equals(new CommandResult("feedback", true, false, false, false, false,
@@ -56,6 +61,159 @@ public class CommandResultTest {
         // different showPayments value -> returns false
         assertFalse(commandResult.equals(new CommandResult("feedback", false, false, false, false, false,
                 false, true)));
+    }
+
+    @Test
+    public void equals_isShowHelp_success() {
+        CommandResult commandResult = new CommandResult("feedback", true, false, false, false, false,
+                false, false);
+
+        // same showHelp value -> returns true
+        assertTrue(commandResult.equals(new CommandResult("feedback", true, false, false, false, false,
+                false, false)));
+
+        // isShowHelp -> return true
+        assertTrue(commandResult.isShowHelp());
+
+        // Everything else -> return false
+        assertFalse(commandResult.isExit());
+        assertFalse(commandResult.isShowContacts());
+        assertFalse(commandResult.isShowExpenses());
+        assertFalse(commandResult.isShowGroups());
+        assertFalse(commandResult.isShowPaymentsCommand());
+        assertFalse(commandResult.isShowTransactionSummary());
+    }
+
+    @Test
+    public void equals_isExit_success() {
+        CommandResult commandResult = new CommandResult("feedback", false, true, false, false, false,
+                false, false);
+
+
+        // different exit value -> returns false
+        assertTrue(commandResult.equals(new CommandResult("feedback", false, true, false, false,
+                false, false, false)));
+
+        // isExit -> return true
+        assertTrue(commandResult.isExit());
+
+        // Everything else -> return false
+        assertFalse(commandResult.isShowHelp());
+        assertFalse(commandResult.isShowContacts());
+        assertFalse(commandResult.isShowExpenses());
+        assertFalse(commandResult.isShowGroups());
+        assertFalse(commandResult.isShowPaymentsCommand());
+        assertFalse(commandResult.isShowTransactionSummary());
+    }
+
+    @Test
+    public void equals_isShowGroup_success() {
+        CommandResult commandResult = new CommandResult("feedback", false, false, true, false, false,
+                false, false);
+
+
+        // different showGroup value -> returns false
+        assertTrue(commandResult.equals(new CommandResult("feedback", false, false, true, false, false,
+                false, false)));
+
+        // isShowGroup -> return true
+        assertTrue(commandResult.isShowGroups());
+
+        // Everything else -> return false
+        assertFalse(commandResult.isShowHelp());
+        assertFalse(commandResult.isShowContacts());
+        assertFalse(commandResult.isShowExpenses());
+        assertFalse(commandResult.isExit());
+        assertFalse(commandResult.isShowPaymentsCommand());
+        assertFalse(commandResult.isShowTransactionSummary());
+    }
+
+    @Test
+    public void equals_isShowContact_success() {
+        CommandResult commandResult = new CommandResult("feedback", false, false, false, true, false,
+                false, false);
+
+
+        // different showContacts value -> returns false
+        assertTrue(commandResult.equals(new CommandResult("feedback", false, false, false, true, false,
+                false, false)));
+
+        // isShowContact -> return true
+        assertTrue(commandResult.isShowContacts());
+
+        // Everything else -> return false
+        assertFalse(commandResult.isShowHelp());
+        assertFalse(commandResult.isExit());
+        assertFalse(commandResult.isShowExpenses());
+        assertFalse(commandResult.isShowGroups());
+        assertFalse(commandResult.isShowPaymentsCommand());
+        assertFalse(commandResult.isShowTransactionSummary());
+    }
+
+    @Test
+    public void equals_isShowExpense_success() {
+        CommandResult commandResult = new CommandResult("feedback", false, false, false, false, true,
+                false, false);
+
+
+        // different showExpenses value -> returns false
+        assertTrue(commandResult.equals(new CommandResult("feedback", false, false, false, false, true,
+                false, false)));
+
+        // isShowExpense -> return true
+        assertTrue(commandResult.isShowExpenses());
+
+        // Everything else -> return false
+        assertFalse(commandResult.isShowHelp());
+        assertFalse(commandResult.isShowContacts());
+        assertFalse(commandResult.isExit());
+        assertFalse(commandResult.isShowGroups());
+        assertFalse(commandResult.isShowPaymentsCommand());
+        assertFalse(commandResult.isShowTransactionSummary());
+    }
+
+    @Test
+    public void equals_isShowTransaction_success() {
+        CommandResult commandResult = new CommandResult("feedback", false, false, false, false, false,
+                true, false);
+
+
+        // different showTransactionSummary value -> returns false
+        assertTrue(commandResult.equals(new CommandResult("feedback", false, false, false, false, false,
+                true, false)));
+
+        // isShowTransaction -> return true
+        assertTrue(commandResult.isShowTransactionSummary());
+
+        // Everything else -> return false
+        assertFalse(commandResult.isShowHelp());
+        assertFalse(commandResult.isShowContacts());
+        assertFalse(commandResult.isShowExpenses());
+        assertFalse(commandResult.isShowGroups());
+        assertFalse(commandResult.isShowPaymentsCommand());
+        assertFalse(commandResult.isExit());
+    }
+
+    @Test
+    public void equals_isShowPayments_success() {
+        CommandResult commandResult = new CommandResult("feedback", false, false, false, false, false,
+                false, true);
+
+
+        // different showPayments value -> returns false
+        assertTrue(commandResult.equals(new CommandResult("feedback", false, false, false, false, false,
+                false, true)));
+
+        // isShowPayement -> return true
+        assertTrue(commandResult.isShowPaymentsCommand());
+
+        // Everything else -> return false
+        assertFalse(commandResult.isShowHelp());
+        assertFalse(commandResult.isShowContacts());
+        assertFalse(commandResult.isShowExpenses());
+        assertFalse(commandResult.isShowGroups());
+        assertFalse(commandResult.isExit());
+        assertFalse(commandResult.isShowTransactionSummary());
     }
 
     @Test
