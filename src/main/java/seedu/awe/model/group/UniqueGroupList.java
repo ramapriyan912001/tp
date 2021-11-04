6efc0e8553ca174a5b019c19040e8abe5d19e1e7
Jingwencloud <jingwen098765432@gmail.com>
2021-11-04 14:07:04 +0800

    Add tests for groups model

--- src/main/java/seedu/awe/model/group/UniqueGroupList.java
+++ src/main/java/seedu/awe/model/group/UniqueGroupList.java
@@ -126,7 +126,7 @@ public class UniqueGroupList implements Iterable<Group> {
     public void setGroups(List<Group> groups) {
         requireAllNonNull(groups);
         if (!groupsAreUnique(groups)) {
-            throw new DuplicatePersonException();
+            throw new DuplicateGroupException();
         }
 
         internalList.setAll(groups);
@@ -165,7 +165,7 @@ public class UniqueGroupList implements Iterable<Group> {
     /**
      * Returns true if {@code groups} contains only unique groups.
      */
-    private boolean groupsAreUnique(List<Group> groups) {
+    public boolean groupsAreUnique(List<Group> groups) {
         for (int i = 0; i < groups.size() - 1; i++) {
             for (int j = i + 1; j < groups.size(); j++) {
                 if (groups.get(i).isSameGroup(groups.get(j))) {
--- src/test/java/seedu/awe/model/group/GroupNameTest.java
+++ src/test/java/seedu/awe/model/group/GroupNameTest.java
@@ -22,25 +22,54 @@ public class GroupNameTest {
     }
 
     @Test
+    public void constructor_tooLongGroupName_throwsIllegalArgumentException() {
+        String invalidName = "GroupNameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+        assertThrows(IllegalArgumentException.class, () -> new Name(invalidName));
+    }
+
+    @Test
     public void isValidGroupName() {
-        // null name
+        // null group name
         assertThrows(NullPointerException.class, () -> GroupName.isValidGroupName(null));
 
-        // invalid name
-        assertFalse(Name.isValidGroupName("")); // empty string
-        assertFalse(Name.isValidGroupName(" ")); // spaces only
-        assertFalse(Name.isValidGroupName("^")); // only non-alphanumeric characters
-        assertFalse(Name.isValidGroupName("bali*")); // contains non-alphanumeric characters
-        assertFalse(Name.isValidGroupName("123456789012345678901234567"
+        // invalid group name
+        assertFalse(GroupName.isValidGroupName("")); // empty string
+        assertFalse(GroupName.isValidGroupName(" ")); // spaces only
+        assertFalse(GroupName.isValidGroupName("^")); // only non-alphanumeric characters
+        assertFalse(GroupName.isValidGroupName("bali*")); // contains non-alphanumeric characters
+        assertFalse(GroupName.isValidGroupName("123456789012345678901234567"
                 + "890123456789012345678901")); // contains > 50 characters
 
-        // valid name
-        assertTrue(Name.isValidGroupName("hong kong")); // alphabets only
-        assertTrue(Name.isValidGroupName("12345")); // numbers only
-        assertTrue(Name.isValidGroupName("budapest 2nd time")); // alphanumeric characters
-        assertTrue(Name.isValidGroupName("Capital Bangkok")); // with capital letters
-        assertTrue(Name.isValidGroupName("The Grand Canyon and Niagara Falls")); // long names
-        assertTrue(Name.isValidGroupName("123456789012345678901234567"
+        // valid group name
+        assertTrue(GroupName.isValidGroupName("hong kong")); // alphabets only
+        assertTrue(GroupName.isValidGroupName("12345")); // numbers only
+        assertTrue(GroupName.isValidGroupName("budapest 2nd time")); // alphanumeric characters
+        assertTrue(GroupName.isValidGroupName("Capital Bangkok")); // with capital letters
+        assertTrue(GroupName.isValidGroupName("The Grand Canyon and Niagara Falls")); // long names
+        assertTrue(GroupName.isValidGroupName("123456789012345678901234567"
                 + "89012345678901234567890")); // contains 50 characters
+    }
+
+    @Test
+    public void equals() {
+        GroupName groupName = new GroupName("Africa");
+
+        // same instance -> true
+        assertTrue(groupName.equals(groupName));
+
+        // null -> false
+        assertFalse(groupName.equals(null));
+
+        // not instance of GroupName -> false
+        assertFalse(groupName.equals(new Name("Africa")));
+
+        // String is passed in -> false
+        assertFalse(groupName.equals("Africa"));
+
+        // different group name -> return false
+        assertFalse(groupName.equals(new GroupName("South Africa")));
+
+        // different case -> return false
+        assertFalse(groupName.equals(new GroupName("africa")));
     }
 }
