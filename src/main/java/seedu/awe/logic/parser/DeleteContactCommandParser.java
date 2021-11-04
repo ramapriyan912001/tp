875630be14c3bbba39e1955ba44ee3b3a4f2be88
Jingwencloud <jingwen098765432@gmail.com>
2021-11-04 14:07:42 +0800

    Fix bug of wrong error message shown when integer is too big

--- src/main/java/seedu/awe/logic/parser/DeleteContactCommandParser.java
+++ src/main/java/seedu/awe/logic/parser/DeleteContactCommandParser.java
@@ -1,8 +1,9 @@
 package seedu.awe.logic.parser;
 
-import static seedu.awe.commons.core.Messages.MESSAGE_DELETECONTACTCOMMAND_USAGE;
-import static seedu.awe.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.awe.commons.core.Messages.*;
+import static seedu.awe.logic.parser.ParserUtil.MESSAGE_INVALID_LENGTH_INDEX;
 
+import seedu.awe.commons.core.Messages;
 import seedu.awe.commons.core.index.Index;
 import seedu.awe.logic.commands.DeleteContactCommand;
 import seedu.awe.logic.parser.exceptions.ParseException;
@@ -22,9 +23,14 @@ public class DeleteContactCommandParser implements Parser<DeleteContactCommand> 
             Index index = ParserUtil.parseIndex(args);
             return new DeleteContactCommand(index);
         } catch (ParseException pe) {
-            throw new ParseException(
+            String exceptionMessage = pe.getMessage();
+            if (exceptionMessage.equals(MESSAGE_INVALID_LENGTH_INDEX)) {
+                throw new ParseException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            } else {
+                throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_DELETECONTACTCOMMAND_USAGE), pe);
+            }
         }
     }
-
 }
+
